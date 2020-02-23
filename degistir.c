65906cbcdb302a72710821c20df6744ca99f43ed
suheylakarakaya <45814285+suheylakarakaya@users.noreply.github.com>
2020-02-23 22:36:31 +0300

    Update degistir.c

--- degistir.c
+++ degistir.c
@@ -1,12 +1,13 @@
+//İki sayinin yerinin degistirilmesini saglayan program//
 #include <stdio.h>
 
 int main(){
-	float sayi1, sayi2;
-	float gecici;
+	float sayi1, sayi2;   //float(reel sayilar) 
+	float gecici;         
 	printf("Iki sayi girin: ");
 	scanf("%f%f", &sayi1, &sayi2);
 	printf("Sayilarin ilk degerleri: %f %f\n", sayi1, sayi2);
-	gecici = sayi1;
+	gecici = sayi1;       
 	sayi1 = sayi2;
 	sayi2 = gecici;
 	printf("Sayilarin son degerleri: %f %f\n", sayi1, sayi2);
