ad4cc0bbb26e730a0237f72e9322120ebb0ae3b1
suheylakarakaya <suheylakarakaya765@gmail.com>
2020-02-23 22:28:52 +0300

    C 101

---
+++ degistir.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+	float sayi1, sayi2;
+	float gecici;
+	printf("Iki sayi girin: ");
+	scanf("%f%f", &sayi1, &sayi2);
+	printf("Sayilarin ilk degerleri: %f %f\n", sayi1, sayi2);
+	gecici = sayi1;
+	sayi1 = sayi2;
+	sayi2 = gecici;
+	printf("Sayilarin son degerleri: %f %f\n", sayi1, sayi2);
+	system("pause");
+	return 0;
+}
