4d4add8c60be6a32bf79ddc60c880e01cf1a9e49
suheylakarakaya <suheylakarakaya765@gmail.com>
2020-04-12 17:21:36 +0300

    update2_merhaba.c

--- 1_merhaba.c
+++ 1_merhaba.c
@@ -1,7 +1,7 @@
 #include <stdio.h>   // standart input-output kütüphesi
 
 int main() { 	     // ana fonksiyon
-	printf("Merhaba dunya!\n"); //Ekrana verileri yazmak içim kullanılan bir fonksiyondur.
+	printf("Merhaba dunya!!\n"); //Ekrana verileri yazmak içim kullanılan bir fonksiyondur.
 	system("pause");	    //Devc++ ide sini kullanıyorsanız, program çalıştıktan sonra terminal penceresinin kapanmasını önler. 
 	return 0;
 }
